e654ffa9a8042712edd52aa53bbd9db9e63ed644
Stefan Pla <spla@icloud.com>
2025-04-21 13:21:19 -0400

    added homepage images, minimal styling

--- jbb_site/src/sections/HomepageImage.jsx
+++ jbb_site/src/sections/HomepageImage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Section.css';
+import image from '../assets/landing-page-image.jpg';
+import signatureImage from '../assets/brayton-signature.jpg';
 
 function HomepageImage() {
   return (
     <section className="section homepage-image">
-        <h1>Welcome to the site</h1>
+        <img src={signatureImage} alt="signature" className="signature-img" />
+        <img src ={image} alt="landing-page-image" className="landing-page-img"></img>
     </section>
   )
 }
