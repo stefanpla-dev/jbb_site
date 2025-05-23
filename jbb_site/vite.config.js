3fcdd079558d6956ef9ab804b289b76c9f913131
Stefan Pla <spla@icloud.com>
2025-05-23 10:12:39 -0400

    tweaked vite.config.js for vercel

--- jbb_site/vite.config.js
+++ jbb_site/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
+  base: '/',
+});
